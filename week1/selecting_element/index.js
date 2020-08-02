c3fcdb5193a64aa50e974cb6363623fc56b2d2ab
chadpiper <chad.piper@bryanuniversity.edu>
2020-08-01 23:17:28 -0700

    Add files via upload

---
+++ week1/selecting_element/index.js
@@ -0,0 +1,7 @@
+//adding id
+document.getElementById("help");
+var help = document.getElementById("help");
+console.log(help);
+//adding help
+var t = document.createTextNode("Help");
+  help.appendChild(t);
