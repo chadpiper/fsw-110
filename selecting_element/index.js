63910875f1152d9dc1a920bbe61b8dfac0e68a63
chadpiper <chad.piper@bryanuniversity.edu>
2020-08-02 00:52:38 -0700

    Delete index.js

--- selecting_element/index.js
+++
@@ -1,7 +0,0 @@
-//adding id
-document.getElementById("help");
-var help = document.getElementById("help");
-console.log(help);
-//adding help
-var t = document.createTextNode("Help");
-  help.appendChild(t);
