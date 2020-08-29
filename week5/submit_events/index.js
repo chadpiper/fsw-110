ec6875d46ee7037c18d1af8ceda158058a28a0aa
Chad Piper <chadpiper@bryanuniversity.edu>
2020-08-28 18:52:15 -0700

    pnpw5

---
+++ week5/submit_events/index.js
@@ -0,0 +1,6 @@
+function display(){
+var x=document.details.fName.value;
+var y=document.details.lName.value;
+var z=document.details.phNum.value;
+alert("First Name: "+x+"   "+" Last Name: "+y+"   "+" Phone Number: "+z);
+}
