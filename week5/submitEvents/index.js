9b20a76d6f22eb1edfd9a05c0a2e776d870600f8
Chad Piper <chadpiper@bryanuniversity.edu>
2020-08-29 21:44:07 -0700

    submitEvents

---
+++ week5/submitEvents/index.js
@@ -0,0 +1,16 @@
+function display(){
+    var a=document.details.fName.value;
+    var b=document.details.lName.value;
+    var c=document.details.num.value;
+    var d=document.details.male.value;
+    var e=document.details.female.value;
+    var f=document.details.location.value;
+    var g=document.details.veg.value;
+    var h=document.details.kosher.value;
+    var i=document.details.lactose.value;
+
+    alert( "First Name: "+a+"   "+" Last Name: "+b+"   "+" Age: "+c+"  "+"  Gender: "+d+"  "+" Location: "+f+"  "+" Dietary Restrictions: "+g
+
+    
+    );
+    }
